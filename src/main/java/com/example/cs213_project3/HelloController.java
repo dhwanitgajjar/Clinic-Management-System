ed35007ecf26e9f825a9540405919685a6762898
BrainScum <harb517.hc03@gmail.com>
2024-10-24 00:24:34 -0400

    javaFX base files

---
+++ src/main/java/com/example/cs213_project3/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.cs213_project3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
